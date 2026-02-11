e827ce73245a63ada4cb4885c13556b09d9beeeb
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-02-11 18:50:03 +0100

    Refactor Commons class imports and move getRlSource method

--- src/main/java/io/github/luckymcdev/common/Commons.java
+++ src/main/java/io/github/luckymcdev/common/Commons.java
@@ -5,11 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
@@ -22,6 +18,10 @@ public interface Commons {
         return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 
+    static String getRlSource(ResourceLocation location) {
+        return getRlSource(location, StandardCharsets.UTF_8);
+    }
+
     static String getRlSource(ResourceLocation location, Charset charset) {
         try (InputStream stream = Instances.getResourceManager().getResourceOrThrow(location).open();
              Reader reader = new InputStreamReader(stream, charset);
@@ -31,9 +31,5 @@ public interface Commons {
             LOGGER.error(e.getLocalizedMessage());
             return "";
         }
-    }
-
-    static String getRlSource(ResourceLocation location) {
-        return getRlSource(location, StandardCharsets.UTF_8);
     }
 }
