1af9d2f2a6b5b988cba2780c3d8853aa42c49810
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-02-20 19:05:45 +0100

    Fix OnlyIn, since it throws some sort of Error?

--- src/main/java/io/github/luckymcdev/foundryengine/client/package-info.java
+++ src/main/java/io/github/luckymcdev/foundryengine/client/package-info.java
@@ -1,8 +1,4 @@
 /**
  * Client Side Code for FoundryEngine.
  */
-@OnlyIn(Dist.CLIENT)
 package io.github.luckymcdev.foundryengine.client;
-
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
--- src/main/java/io/github/luckymcdev/foundryengine/server/package-info.java
+++ src/main/java/io/github/luckymcdev/foundryengine/server/package-info.java
@@ -1,8 +1,4 @@
 /**
  * Server Side Code for FoundryEngine.
  */
-@OnlyIn(Dist.CLIENT)
 package io.github.luckymcdev.foundryengine.server;
-
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
