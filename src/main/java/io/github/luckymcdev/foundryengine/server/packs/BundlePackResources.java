199ffc398896eb16c3e606d706454e73dc8dd9c8
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-03-02 17:41:00 +0100

    Revert "Remove unused method in BundlePackResources"
    This reverts commit 7c33dd653fc4264d09db778d220e4ad7d03c3d2f.

--- src/main/java/io/github/luckymcdev/foundryengine/server/packs/BundlePackResources.java
+++ src/main/java/io/github/luckymcdev/foundryengine/server/packs/BundlePackResources.java
@@ -98,6 +98,13 @@ public class BundlePackResources extends AbstractPackResources {
         return namespaces;
     }
 
+    private String packTypeName() {
+        return switch (packType) {
+            case CLIENT_RESOURCES -> "Assets";
+            case SERVER_DATA -> "Data";
+        };
+    }
+
     private String generatePackMeta() {
         PackFormat format = SharedConstants.getCurrentVersion().packVersion(packType);
         int major = format.major();
@@ -115,6 +122,5 @@ public class BundlePackResources extends AbstractPackResources {
 
     @Override
     public void close() {
-        roots.clear();
     }
 }
