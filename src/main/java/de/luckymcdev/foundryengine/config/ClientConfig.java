8cb1e441416ee4b00a6dc7c55d2dfb3c46ea8f5a
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-03-28 12:27:07 +0100

    Add funny offhand rendering, togglable via config.

--- src/main/java/de/luckymcdev/foundryengine/config/ClientConfig.java
+++ src/main/java/de/luckymcdev/foundryengine/config/ClientConfig.java
@@ -7,6 +7,10 @@ public final class ClientConfig extends EngineConfig {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
 
+    public static final ModConfigSpec.BooleanValue RENDER_OFFHAND =
+            BUILDER.comment("If the offhand should be rendered the same way the main hand is being.")
+                    .define("RENDER_OFFHAND", false);
+
     @Override
     public ModConfigSpec spec() {
         return BUILDER.build();
---
+++ src/main/java/de/luckymcdev/foundryengine/mixin/render/ItemInHandRendererMixin.java
@@ -0,0 +1,37 @@
+package de.luckymcdev.foundryengine.mixin.render;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.luckymcdev.foundryengine.config.ClientConfig;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemInHandRenderer.class)
+public abstract class ItemInHandRendererMixin {
+
+    @Invoker("renderPlayerArm")
+    abstract void invokeRenderPlayerArm(PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int light,
+                                        float equippedProgress, float swingProgress, HumanoidArm arm);
+
+    @Inject(method = "renderArmWithItem", at = @At("TAIL"))
+    private void renderOffhandArmWhenEmpty(AbstractClientPlayer player, float f, float g, InteractionHand hand, float swingProgress, ItemStack stack, float equippedProgress,
+                                           PoseStack poseStack, SubmitNodeCollector submitNodeCollector,
+                                           int light, CallbackInfo ci) {
+
+
+        if (!ClientConfig.RENDER_OFFHAND.getAsBoolean()) return;
+
+        if (hand == InteractionHand.OFF_HAND && stack.isEmpty()) {
+            HumanoidArm offArm = player.getMainArm().getOpposite();
+            invokeRenderPlayerArm(poseStack, submitNodeCollector, light, equippedProgress, swingProgress, offArm);
+        }
+    }
+}
