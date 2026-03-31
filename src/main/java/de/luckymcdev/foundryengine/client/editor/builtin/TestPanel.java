94eac8d6f485553c6262585fe19b13d818ea255f
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-03-31 15:34:32 +0200

    Add random stuff to TestPanel

--- src/main/java/de/luckymcdev/foundryengine/client/editor/builtin/TestPanel.java
+++ src/main/java/de/luckymcdev/foundryengine/client/editor/builtin/TestPanel.java
@@ -8,9 +8,12 @@ import de.luckymcdev.foundryengine.client.ui.ExampleScreen;
 import de.luckymcdev.foundryengine.client.util.key.Shortcut;
 import de.luckymcdev.foundryengine.common.Common;
 import de.luckymcdev.foundryengine.common.network.TestPacket;
+import de.luckymcdev.foundryengine.common.util.color.Color;
 import imgui.ImGui;
 import imgui.extension.imguiknobs.ImGuiKnobs;
+import imgui.flag.ImGuiColorEditFlags;
 import imgui.flag.ImGuiKey;
+import imgui.type.ImBoolean;
 import imgui.type.ImFloat;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 
@@ -20,7 +23,9 @@ import net.neoforged.neoforge.client.network.ClientPacketDistributor;
  */
 public class TestPanel extends Panel {
     public static final TestPanel INSTANCE = new TestPanel();
-    private final ImFloat knobValue = new ImFloat(0.5f);
+    private static final ImFloat FLOAT = new ImFloat();
+    private static final ImFloat FLOAT2 = new ImFloat();
+    private static final ImBoolean BOOLEAN = new ImBoolean();
 
     private TestPanel() {
         super(Common.id("test_panel"), "My Panel", Shortcut.ctrl(ImGuiKey.F5));
@@ -42,16 +47,27 @@ public class TestPanel extends Panel {
 
         EngineImGuiUtils.resourceLocation(Common.id("imguiiscool"));
 
-        ImGuiKnobs.knob("This Will Damage you if you press that button!", knobValue, 0.0f, 10.0f);
+        ImGuiKnobs.knob("This Will Damage you if you press that button!", FLOAT, 0.0f, 10.0f);
 
+        ImGui.progressBar(FLOAT.get() / 10, ImGui.getContentRegionAvailX(), 0, "Progress");
 
         if (ImGui.button("Press this!")) {
-            ClientPacketDistributor.sendToServer(new TestPacket(knobValue.get()));
+            ClientPacketDistributor.sendToServer(new TestPacket(FLOAT.get()));
         }
 
         if (ImGui.button("Open Screen")) {
             Client.setScreen(new ExampleScreen());
         }
+
+        if (ImGui.radioButton("Radio goo goo radio ga ga", BOOLEAN.get())) {
+            BOOLEAN.set(!BOOLEAN.get());
+        }
+
+        if (ImGui.colorButton("Color Button?", Color.ORANGE.r(), Color.ORANGE.g(), Color.ORANGE.b(), Color.ORANGE.a())) {
+            ImGui.text("COLOR BUTTON");
+        }
+
+        ImGui.colorPicker3("Pick a color", FLOAT2.getData(), ImGuiColorEditFlags.PickerHueWheel);
 
     }
 }
