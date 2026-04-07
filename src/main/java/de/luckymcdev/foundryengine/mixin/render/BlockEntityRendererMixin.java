dfa575567c533a5c9c451277616d427f9695829e
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-04-07 14:28:49 +0200

    Remove unused LevelRenderer import and variable

--- src/main/java/de/luckymcdev/foundryengine/mixin/render/BlockEntityRendererMixin.java
+++ src/main/java/de/luckymcdev/foundryengine/mixin/render/BlockEntityRendererMixin.java
@@ -2,7 +2,6 @@ package de.luckymcdev.foundryengine.mixin.render;
 
 import de.luckymcdev.foundryengine.config.ClientConfig;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -18,8 +17,6 @@ public interface BlockEntityRendererMixin {
     default int getViewDistance() {
         int rd = Minecraft.getInstance().options.renderDistance().get() * 16;
         String val = ClientConfig.BLOCK_ENTITY_RENDER_DISTANCE.get();
-
-        LevelRenderer levelRenderer = Minecraft.getInstance().levelRenderer;
         return switch (val) {
             case "full" -> rd;
             case "half" -> rd / 2;
