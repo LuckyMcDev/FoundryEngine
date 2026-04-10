4e95bed51e42673d1623c8b92dafcb0c2902a19b
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-04-10 18:15:37 +0200

    Fix Minimal and add bundle icons

--- ExampleBundles/minimal/minimal/Minimal.groovy
+++ ExampleBundles/minimal/minimal/Minimal.groovy
@@ -1,5 +1,9 @@
 package minimal
 
+import de.luckymcdev.foundryengine.common.bundle.config.BundleConfig
+import de.luckymcdev.foundryengine.common.script.BundleEntrypoint
+import net.neoforged.bus.api.IEventBus
+
 class Minimal extends BundleEntrypoint {
 
     Minimal(IEventBus eventBus, BundleConfig bundleConfig) {
