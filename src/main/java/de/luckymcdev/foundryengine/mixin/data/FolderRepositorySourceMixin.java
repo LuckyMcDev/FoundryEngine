b9a543e4abd788d451958062f40ed30a38564eea
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-04-29 17:53:52 +0200

    Remove anonymous class from PackResourcesSupplier

--- src/main/java/de/luckymcdev/foundryengine/mixin/data/FolderRepositorySourceMixin.java
+++ src/main/java/de/luckymcdev/foundryengine/mixin/data/FolderRepositorySourceMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.FolderRepositorySource;
 import net.minecraft.server.packs.repository.Pack;
 import net.neoforged.neoforge.common.NeoForge;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,20 +29,22 @@ public class FolderRepositorySourceMixin {
     private void engine$loadPacks(Consumer<Pack> pOnLoad, CallbackInfo ci) {
         List<PackResources> packs = NeoForge.EVENT_BUS.post(new RegisterVirtualPackEvent.BeforeUser()).getPacks();
         for (PackResources pack : packs) {
-            pOnLoad.accept(Objects.requireNonNull(Pack.readMetaAndCreate(pack.location(), new Pack.ResourcesSupplier() {
-                        @Override
-                        public @NotNull PackResources openPrimary(@NotNull PackLocationInfo pLocation) {
-                            return pack;
-                        }
-
-                        @Override
-                        public @NotNull PackResources openFull(@NotNull PackLocationInfo pLocation, Pack.@NotNull Metadata pMetadata) {
-                            return pack;
-                        }
-                    },
+            pOnLoad.accept(Objects.requireNonNull(Pack.readMetaAndCreate(pack.location(), new PackResourcesSupplier(pack),
                     this.packType,
                     new PackSelectionConfig(true, Pack.Position.TOP, false)
             )));
+        }
+    }
+
+    private record PackResourcesSupplier(PackResources packResources) implements Pack.ResourcesSupplier {
+        @Override
+        public PackResources openPrimary(PackLocationInfo pLocation) {
+            return this.packResources;
+        }
+
+        @Override
+        public PackResources openFull(PackLocationInfo location, Pack.Metadata metadata) {
+            return this.packResources;
         }
     }
 }
