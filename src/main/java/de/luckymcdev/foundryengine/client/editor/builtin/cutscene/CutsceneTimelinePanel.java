7308afd0ccbbde87daf40e65eddb110542c60633
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-05-05 16:45:44 +0200

    Remove deprecation from CutsceneTimelinePanel since the mention doesnt exist yet.

--- src/main/java/de/luckymcdev/foundryengine/client/editor/builtin/cutscene/CutsceneTimelinePanel.java
+++ src/main/java/de/luckymcdev/foundryengine/client/editor/builtin/cutscene/CutsceneTimelinePanel.java
@@ -35,9 +35,7 @@ import java.util.List;
 
 /**
  * Dedicated cutscene timeline / preview / effects panel.
- * @deprecated Use {@link CutsceneEditorPanel} instead
  */
-@Deprecated
 public class CutsceneTimelinePanel extends EditorPanel {
     public static final CutsceneTimelinePanel INSTANCE = new CutsceneTimelinePanel();
 
