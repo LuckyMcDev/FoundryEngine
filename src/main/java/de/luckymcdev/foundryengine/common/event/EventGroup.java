de4f4fc9f26702d12ba93080b1f9306feb247b9c
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-05-24 14:51:01 +0200

    Try fix IllegalStateException in registry when erroring

--- src/main/java/de/luckymcdev/foundryengine/common/event/EventGroup.java
+++ src/main/java/de/luckymcdev/foundryengine/common/event/EventGroup.java
@@ -1,9 +1,16 @@
 package de.luckymcdev.foundryengine.common.event;
 
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.ModLoader;
+import net.neoforged.fml.ModLoadingIssue;
+import org.slf4j.Logger;
+
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class EventGroup<T> {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
     private final List<EventCallback<T>> listeners = new CopyOnWriteArrayList<>();
 
     public void add(EventCallback<T> callback) {
@@ -12,7 +19,14 @@ public class EventGroup<T> {
 
     public void post(T event) {
         for (EventCallback<T> listener : listeners) {
-            listener.execute(event);
+            try {
+                listener.execute(event);
+            } catch (Exception e) {
+                LOGGER.error("Uncaught error in event callback", e);
+                ModLoadingIssue issue = ModLoadingIssue.error(
+                        "Uncaught error in event callback: " + e.getMessage());
+                ModLoader.addLoadingIssue(issue);
+            }
         }
     }
 
