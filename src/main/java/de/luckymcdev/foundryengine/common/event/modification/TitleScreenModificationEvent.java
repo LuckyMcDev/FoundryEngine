9fa51bcccc05c65451c7b86b405b37ccd1e4875f
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-06-19 17:26:54 +0200

    refactor(events): TitleScreenModifyEvent to TitleScreenModificationEvent and move to modification package

--- src/main/java/de/luckymcdev/foundryengine/common/event/modification/TitleScreenModificationEvent.java
+++ src/main/java/de/luckymcdev/foundryengine/common/event/modification/TitleScreenModificationEvent.java
@@ -1,12 +1,12 @@
-package de.luckymcdev.foundryengine.common.event;
+package de.luckymcdev.foundryengine.common.event.modification;
 
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.ICancellableEvent;
 
-public class TitleScreenModifyEvent extends Event implements ICancellableEvent {
+public class TitleScreenModificationEvent extends Event implements ICancellableEvent {
     private final ButtonType buttonType;
 
-    public TitleScreenModifyEvent(ButtonType buttonType) {
+    public TitleScreenModificationEvent(ButtonType buttonType) {
         this.buttonType = buttonType;
     }
 
