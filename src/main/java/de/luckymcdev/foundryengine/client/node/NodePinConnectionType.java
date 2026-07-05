d396ba2fee2ebacc86d54b8b8d4dc6ae282f3074
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-07-05 11:34:47 +0200

    refactor(node): improve pin list handling in Node and NodeEditorInstance

--- src/main/java/de/luckymcdev/foundryengine/client/node/NodePinConnectionType.java
+++ src/main/java/de/luckymcdev/foundryengine/client/node/NodePinConnectionType.java
@@ -1,12 +1,13 @@
 package de.luckymcdev.foundryengine.client.node;
 
 public enum NodePinConnectionType {
-    OUTPUT("output"),
-    REQUIRED_INPUT("required_input"),
-    OPTIONAL_INPUT("optional_input");
-    public final String name;
+	OUTPUT("output"),
+	REQUIRED_INPUT("required_input"),
+	OPTIONAL_INPUT("optional_input");
 
-    NodePinConnectionType(String name) {
-        this.name = name;
-    }
+	public final String id;
+
+	NodePinConnectionType(String id) {
+		this.id = id;
+	}
 }
