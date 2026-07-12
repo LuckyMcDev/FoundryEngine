e2629119064663aac109dfca1ec6b7513cba0386
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-07-12 14:34:15 +0200

    chore(nullability): make signature nullable in AutocompleteItem

--- src/main/java/de/luckymcdev/foundryengine/client/imgui/text/autocomplete/AutocompleteItem.java
+++ src/main/java/de/luckymcdev/foundryengine/client/imgui/text/autocomplete/AutocompleteItem.java
@@ -1,9 +1,10 @@
 package de.luckymcdev.foundryengine.client.imgui.text.autocomplete;
 
 import de.luckymcdev.foundryengine.common.util.color.Color;
+import org.jspecify.annotations.Nullable;
 
 /**
  * @param type "keyword", "type", "function", "variable", "constant"
  */
-public record AutocompleteItem(String text, String type, String signature, Color color) {
+public record AutocompleteItem(String text, String type, @Nullable String signature, Color color) {
 }
